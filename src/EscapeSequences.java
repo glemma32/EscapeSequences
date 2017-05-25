a71d8e4d39d860e1e7bc24a62f766e7fd3fa5150
glemma32 <genet.woldeyes@gmail.com>
2017-05-25 17:35:59 -0400

    First commit

---
+++ src/EscapeSequences.java
@@ -0,0 +1,26 @@
+
+public class EscapeSequences {
+	public static void main(String[] args){
+		
+		System.out.println("  ***\n");
+		System.out.println(" *   *\n");
+		System.out.println("*     *\n");
+		System.out.println("*     *\n");
+		System.out.println(" *   *\n");
+		System.out.println("  ***\n\n\n\n");
+		//System.out.println("\t\n\r*");
+		
+		System.out.println(" ********\n");
+		System.out.println(" *      *\n");
+		System.out.println(" *      *\n");
+		System.out.println(" *      *\n");
+		System.out.println(" *      *\n");
+		System.out.println(" ********\n");
+		
+			}
+
+}
+ 
+
+ 
+
